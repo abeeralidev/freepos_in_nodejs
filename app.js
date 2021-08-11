9e93e0850a770f3fb839eb61cd695e76c7cb959b
atta <attachaudhury@gmail.com>
2021-08-11 22:45:01 +0500

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressLayouts = require('express-ejs-layouts');
+var engine = require('ejs-locals')
 
 
 var indexRouter = require('./routes/main/index');
@@ -28,6 +28,7 @@ const port = 8016
 var app = express();
 
 // view engine setup
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -37,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expressLayouts);
+
 
 
 app.use('/', indexRouter);
